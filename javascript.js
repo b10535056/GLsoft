2e2e7b315726fb62c607b5a83feb9f4b388c3eb2
Ethan_Chen <b10535056@gemail.yuntech.edu.tw>
2022-06-30 12:52:01 +0800

    fifth modified

--- javascript.js
+++ javascript.js
@@ -1,18 +1,16 @@
 const navbar = document.querySelector(".navbar");
 const nav = document.querySelector(".nav");
-const navbtn = document.getElementsByClassName("navbtn");
+const navbtn = document.querySelector(".navbtn");
+const navbtns = document.getElementsByClassName("navbtn");
 
 navbar.addEventListener("click", () => {
   nav.classList.toggle("fadeout");
 });
 
-for (var i = 0; i < navbtn.length; i++) {
-  const btnnav = document.querySelector(".navbtn");
-  console.log(btnnav);
-
-  btnnav.addEventListener("click", (event) => {
-    const btn = event.target;
-    const ulface = btn.parentElement.parentElement;
-    console.log(ulface);
+for (var i = 0; i < navbtns.length; i++) {
+  const navbutton = navbtns[i];
+  navbutton.addEventListener("click", () => {
+    nav.classList.toggle("fadeout");
   });
+  console.log(navbutton);
 }
