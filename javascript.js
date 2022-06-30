b867e0e691f8908fdf4819408353a2ae4bf83f32
Ethan_Chen <b10535056@gemail.yuntech.edu.tw>
2022-06-30 12:43:08 +0800

    fourth modified

--- javascript.js
+++ javascript.js
@@ -1,6 +1,18 @@
 const navbar = document.querySelector(".navbar");
 const nav = document.querySelector(".nav");
+const navbtn = document.getElementsByClassName("navbtn");
 
 navbar.addEventListener("click", () => {
   nav.classList.toggle("fadeout");
 });
+
+for (var i = 0; i < navbtn.length; i++) {
+  const btnnav = document.querySelector(".navbtn");
+  console.log(btnnav);
+
+  btnnav.addEventListener("click", (event) => {
+    const btn = event.target;
+    const ulface = btn.parentElement.parentElement;
+    console.log(ulface);
+  });
+}
